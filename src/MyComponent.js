88943f2cad9bdf123ec6ca225682a464d6b69a1b
khd8593 <khd8593@naver.com>
2022-12-19 16:16:54 +0900

    Props

--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,20 @@
 import React from 'react'
-
-const MyComponent = () => {
-	return <div>나의 새롭고 멋진 컴포넌트</div>
+import PropTypes from 'prop-types'
+const MyComponent = ({ name, children }) => {
+	return (
+		<>
+			안녕하세요, 제 이름은 {name}입니다. <br />
+			children 값은 {children}
+			입니다.
+		</>
+	)
 }
 
+MyComponent.defaultProps = {
+	name: '기본 이름'
+}
+
+MyComponent.propTypes = {
+	name: PropTypes.string.isRequired
+}
 export default MyComponent
